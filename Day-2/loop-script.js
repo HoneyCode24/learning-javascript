21a347f83f4f66eb9d37c502530512d55e66364d
Honey Gupta <hg133515@gmail.com>
2025-05-22 14:32:36 +0530

    Completed the loops part and adding the question

--- Day-2/loop-script.js
+++ Day-2/loop-script.js
@@ -1,11 +1,11 @@
-/* print all the even number between 0-100 
+/* Question 1 print all the even number between 0-100 */
 for(let i=0; i<=100;i++){
     if(i%2 == 0){
         console.log(i,"is a even number");
     }
-}*/
+}
 
-/*create a game where you start game with some random and then ask the user to guess the number and the games continues untill the user guess the correct number. 
+/*Question 2 create a game where you start game with some random and then ask the user to guess the number and the games continues untill the user guess the correct number. */
 
 let guess_num = 25;
 user_num = prompt("enter the number :");
@@ -22,9 +22,9 @@ while(user_num != guess_num){
     }
     user_num = prompt("Guess the number again, it doesn't match")
 }
-alert("Congrats you have guess it correct.");*/
+alert("Congrats you have guess it correct.");
 
-/* prompt the user to enter their full name. Generate a username for them based on the input start username with @, followed by their full name and ending with the fullname length*/
+/* Question 3 prompt the user to enter their full name. Generate a username for them based on the input start username with @, followed by their full name and ending with the fullname length*/
 
 let user_name = prompt("Enter YOur Full name :")
 let len = user_name.length 
